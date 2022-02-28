bbe9fdebb139e95c3d34b01f0a3bc5f2d36f7cfe
OrkWard <orkward_o@outlook.com>
2022-02-28 14:25:36 +0800

    add annotation

--- project1.cpp
+++ project1.cpp
@@ -23,12 +23,13 @@ int main() {
     Student Stu[10];
     ifstream InputFile("./input.txt");
 
-    // use three structrue to store the sum, max, and min of score1, score2 and score3.
+    // use three structrues to store the sum, max, and min of score1, score2 and score3.
     Student Sum, Max, Min;
     Sum.score1 = Sum.score2 = Sum.score3 = 0;
     Max.score1 = Max.score2 = Max.score3 = 0;
     Min.score1 = Min.score2 = Min.score3 = 5;
     for (int i = 0; i < 10; i++) {
+        // this conditional branch differs the 1st, 2nd, 3rd and other. 
         if (i == 0) 
             cout << "Please input the 1st student's name";
         else if (i == 1) 
@@ -38,6 +39,7 @@ int main() {
         else
             cout << "Please input the " << i + 1 << "th student's name";
         cout << " and three scores:"; 
+        // input the data from the file, then calculate the sum, max and min real-time
         InputFile >> Stu[i].name >> Stu[i].score1 >> Stu[i].score2 >> Stu[i].score3;
         Sum.score1 += Stu[i].score1;
         Sum.score2 += Stu[i].score2;
@@ -48,11 +50,13 @@ int main() {
         Min.score1 = Stu[i].score1 < Min.score1 ? Stu[i].score1 : Min.score1;
         Min.score2 = Stu[i].score2 < Min.score2 ? Stu[i].score2 : Min.score2;
         Min.score3 = Stu[i].score3 < Min.score3 ? Stu[i].score3 : Min.score3;
+        // an echo for input
         cout << " " << Stu[i].name << " " << Stu[i].score1 << " " << Stu[i].score2 << " " << Stu[i].score3 << endl;
     }
 
     cout << "no      name    score1  score2  score3  average" << endl;
     for (int i = 0; i < 10; i++) {
+        // output fitting to given format
         cout << left << setw(8) << i + 1 << setw(8) << Stu[i].name << setw(8) << Stu[i].score1 << setw(8) << Stu[i].score2 << setw(8) << Stu[i].score3 << setprecision(6) << average(Stu + i) << endl;
     }
 
