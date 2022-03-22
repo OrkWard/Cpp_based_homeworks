dc2314c85c7d1e686c287d7a1bd3fbd56eba88be
OrkWard <orkward_o@outlook.com>
2022-03-22 11:56:43 +0800

    complete the game procedure

--- project3.cpp
+++ project3.cpp
@@ -8,13 +8,16 @@ enum ROOMTYPE {
 	normal, lobby, princess, monster
 };
 
+// direction array
+const string direction[4] = {"up", "down", "west", "east"};
+
 // Store one room's info, room type, whether rooms 
 // around accessable, and whether visited.
 struct room {
 	ROOMTYPE roomType;
 	int doorNum;
 	bool direction[4];
-	bool visited;
+	bool visited = false;
 };
 
 // Store the whole labyrinth
@@ -55,8 +58,13 @@ void Labyrinth::initialize(void) {
 
 }
 
+struct player {
+	int positionX;
+	int positionY;
+	bool meetPrc;
+};
+
 int findDirectNum(string cmd) {
-	const string direction[4] = {"up", "down", "west", "east"};
 	for (int i = 0; i < 4; i++) {
 		if (direction[i] == cmd) return i;
 	}
@@ -65,11 +73,7 @@ int findDirectNum(string cmd) {
 int main() {
 	// only need to record the position 
 	// the player
-	struct {
-		int positionX;
-		int positionY;
-		bool meetPrc;
-	} hero;
+	struct player hero;
 
 	int labySize;
 	cout << "Welcome to Save the Princess." << endl << "Please enter the labyrinth size:" << endl;
@@ -95,20 +99,70 @@ int main() {
 	cout << "Enter your command: ";
 	
 	// the game procedure
+
+}
+
+bool gameProcedure(struct player hero, Labyrinth* laby) {
+	bool withPrc = false;
 	while(1) {
 		string cmd;
 		cin >> cmd;
 		stringstream ss(cmd);
 		ss >> cmd; ss >> cmd;
+
+		// move
 		switch(findDirectNum(cmd)) {
 			case 0: hero.positionY++; break;
 			case 1: hero.positionY--; break;
 			case 2: hero.positionX--; break;
 			case 3: hero.positionX++; break;
 		}
-		if (laby->m_rooms[hero.positionX][hero.positionY].visited) {
+		
+		// if visited
+		if (!laby->m_rooms[hero.positionX][hero.positionY].visited) {
 			cout << "You've entered a new room. ";
+			laby->m_rooms[hero.positionX][hero.positionY].visited = true;
+
+			// different room types
+			if (laby->m_rooms[hero.positionX][hero.positionY].roomType == monster) {
+				cout << "Damn! A monster! You dead.";
+				cout << "Game Over";
+				return 0;
+			}
+			else if (laby->m_rooms[hero.positionX][hero.positionY].roomType == princess) {
+				cout << "Congratulation! You find the secret room!" << endl;
+				cout << "You see the princess. Enter to continue:" << endl;
+				getline(cin, cmd);
+				cout << "You: I will bring you out here, my princess." << endl;
+				getline(cin, cmd);
+				cout << "Princess: I appreciate it. Thank you so much." << endl;
+				getline(cin, cmd);
+				cout << "You: No time to wait. Let's get out now." << endl;
+				getline(cin, cmd);
+			}
 		}
-		cout << "There are " << laby->m_rooms[hero.positionX][hero.positionY].doorNum;
+		else {
+			if (laby->m_rooms[hero.positionX][hero.positionY].roomType == lobby) {
+				if (withPrc) {
+					cout << "You bring the princess out of the labyrinth! You've won the game!";
+					return 0;
+				}
+				else {
+					cout << "You've back to the lobby, but only yourself. Where is the princess?";
+					getline(cin, cmd);
+				}
+			} 
+			else 
+				cout << "You've entered a familiar room. ";
+		}
+
+		// Go to next room.
+		cout << "There are " << laby->m_rooms[hero.positionX][hero.positionY].doorNum << " exit(s)";
+		for (int i = 0; i < 4; i ++) {
+			if (laby->m_rooms[hero.positionX][hero.positionY].direction[i]) {
+				cout << "    " << direction[i];
+			}
+		}
+		cout << endl << "Enter your command: ";
 	}
 }
