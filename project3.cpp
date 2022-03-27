a7b59d5a487566d7f1cfba423ec9612cbda84053
OrkWard <orkward_o@outlook.com>
2022-03-27 10:56:36 +0800

    finish project 3

--- project3.cpp
+++ project3.cpp
@@ -20,8 +20,8 @@ const string direction[4] = {"up", "down", "west", "east"};
 struct room {
 	ROOMTYPE roomType;
 	int doorNum;
-	bool direction[4] = {false, false, false, false};
-	bool visited = false;
+	bool direction[4];
+	bool visited;
 };
 
 // Store the whole labyrinth
@@ -32,12 +32,8 @@ class Labyrinth {
 			for (int i = 0; i < size; i++) {
 				m_rooms[i] = new struct room[size];
 			}
-			for (int i = 0; i < size; i++)
-				for (int j = 0; j < size; j++)
-					m_rooms[i][j].roomType = normal;
 			m_lobbyX = m_size / 2;
 			m_lobbyY = m_size / 2;
-			m_rooms[m_lobbyX][m_lobbyY].roomType = lobby;
 		}
 		~Labyrinth() {
 			for (int i = 0; i < m_size; i++) {
@@ -54,7 +50,7 @@ class Labyrinth {
 	private:
 		void allocPrcMst(void); // this function randomly allocate the princess and monster
 		void allocDoor(void); // this function randomly create the doors
-		int reachable(int positionX, int positionY); // check whether the princess and monster room
+		bool reachable(int positionX, int positionY); // check whether the princess and monster room
 													  // reachable from the given position
 		void printMap(void); // use for debug, print the map
 };
@@ -81,23 +77,30 @@ void Labyrinth::allocDoor(void) {
 	// the door has the possibility of 60%
 	for (int i = 0; i < m_size; i++) {
 		for (int j = 0; j < m_size; j++) {
-			if (i < m_size) m_rooms[i][j].direction[1] = (rand() % 1000)*1.0 / 1000 > 0.4;
-			if (j < m_size) m_rooms[i][j].direction[3] = (rand() % 1000)*1.0 / 1000 > 0.4;
+			if (i < m_size - 1) m_rooms[i][j].direction[1] = (rand() % 1000)*1.0 / 1000 > 0.4;
+			if (j < m_size - 1) m_rooms[i][j].direction[3] = (rand() % 1000)*1.0 / 1000 > 0.4;
+			if (i > 0) m_rooms[i][j].direction[0] = m_rooms[i - 1][j].direction[1];
+			if (j > 0) m_rooms[i][j].direction[2] = m_rooms[i][j - 1].direction[3];
 			m_rooms[i][j].doorNum = m_rooms[i][j].direction[0] + m_rooms[i][j].direction[1] + \
 									m_rooms[i][j].direction[2] + m_rooms[i][j].direction[3];
 		}
 	}
 }
-int Labyrinth::reachable(int positionX, int positionY) {
-	// calculate how many important rooms can be reached
-	int reach = 0;
-	if (m_rooms[positionX][positionY].roomType == princess) return 1;
-	else if (m_rooms[positionX][positionY].roomType == monster) return 1;
+bool Labyrinth::reachable(int positionX, int positionY) {
+	// check if the princess is reachable
+	bool reach = false;
+	m_rooms[positionX][positionY].visited = true;
+	if (m_rooms[positionX][positionY].roomType == monster) return false;
+	else if (m_rooms[positionX][positionY].roomType == princess) return true;
 	else {
-		if (m_rooms[positionX][positionY].direction[0]) reach += reachable(positionX, positionY + 1);
-		if (m_rooms[positionX][positionY].direction[1]) reach += reachable(positionX, positionY - 1);
-		if (m_rooms[positionX][positionY].direction[2]) reach += reachable(positionX - 1, positionY);
-		if (m_rooms[positionX][positionY].direction[3]) reach += reachable(positionX + 1, positionY);
+		if (m_rooms[positionX][positionY].direction[0] && !m_rooms[positionX][positionY].visited)
+			reach |= reachable(positionX - 1, positionY);
+		if (m_rooms[positionX][positionY].direction[1] && !m_rooms[positionX][positionY].visited)
+			reach |= reachable(positionX + 1, positionY);
+		if (m_rooms[positionX][positionY].direction[2] && !m_rooms[positionX][positionY].visited)
+			reach |= reachable(positionX, positionY - 1);
+		if (m_rooms[positionX][positionY].direction[3] && !m_rooms[positionX][positionY].visited)
+			reach |= reachable(positionX, positionY + 1);
 	}
 	return reach;
 }
@@ -106,10 +109,25 @@ void Labyrinth::printMap(void) {
 	HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);
 	// cursor position
 	COORD position;
+	position.X = 0;
+	position.Y = 2;
+	SetConsoleCursorPosition(screen, position);
+	
+	// clear the screen
+	for (int i = 0; i < 5*m_size + 1; i++){
+		for (int j = 0; j < 5*m_size + 1; j++)
+			cout << "  ";
+		cout << endl;
+	}
+
+	SetConsoleCursorPosition(screen, position);
+	for (int i = 0; i < 5*m_size + 1; i++) cout << "[]";
+	cout << endl;
+	for (int i = 0; i < 5*m_size; i++) cout << "[]" << endl;
 	for (int i = 0; i < m_size; i++)
 		for (int j = 0; j < m_size; j++) {
-			position.X = 10*j;
-			position.Y = 5*i + 8;
+			position.X = 10*j + 2;
+			position.Y = 5*i + 7;
 			SetConsoleCursorPosition(screen, position);
 
 			// the horizontal line
@@ -121,14 +139,14 @@ void Labyrinth::printMap(void) {
 			position.Y -= 4;
 			if (m_rooms[i][j].direction[3]) {
 				SetConsoleCursorPosition(screen, position); cout << "[]"; position.Y++;
-				SetConsoleCursorPosition(screen, position); cout << "[]"; position.Y++;
-				SetConsoleCursorPosition(screen, position); cout << "[]"; position.Y++;
+				SetConsoleCursorPosition(screen, position); cout << "  "; position.Y++;
+				SetConsoleCursorPosition(screen, position); cout << "  "; position.Y++;
 				SetConsoleCursorPosition(screen, position); cout << "[]"; 
 			}
 			else {
 				SetConsoleCursorPosition(screen, position); cout << "[]"; position.Y++;
-				SetConsoleCursorPosition(screen, position); cout << " "; position.Y++;
-				SetConsoleCursorPosition(screen, position); cout << " "; position.Y++;
+				SetConsoleCursorPosition(screen, position); cout << "[]"; position.Y++;
+				SetConsoleCursorPosition(screen, position); cout << "[]"; position.Y++;
 				SetConsoleCursorPosition(screen, position); cout << "[]"; 
 			}
 
@@ -145,15 +163,27 @@ void Labyrinth::printMap(void) {
 				cout << "lobby" << endl;
 			}
 		}
+	position.X = 0;
+	position.Y = 5*m_size + 3;
+	SetConsoleCursorPosition(screen, position);
 }
 void Labyrinth::initialize(void) {
 	// allocate princess, monster and door, if princess or monster is
 	// unreachable then reallocate
 	do {
+		for (int i = 0; i < m_size; i++)
+			for (int j = 0; j < m_size; j++) {
+				struct room* tmp = &m_rooms[i][j];
+				tmp->direction[0] = tmp->direction[1] = tmp->direction[2] = tmp->direction[3] = false;
+				tmp->roomType = normal;
+				tmp->visited = false;
+			}
+		m_rooms[m_lobbyX][m_lobbyY].roomType = lobby;
 		allocPrcMst();
 		allocDoor();
-		printMap();
-	} while(reachable(m_lobbyX, m_lobbyY) < 2);
+		// uncomment the next line to enable map
+		// printMap();
+	} while(reachable(m_lobbyX, m_lobbyY));
 }
 
 // get the number of direction
@@ -180,7 +210,7 @@ void gameProcedure(void) {
 		if (labySize < 3) {
 			cout << "Too small! Please enter a larger one: ";
 		}
-		else if (labySize > 20) {
+		else if (labySize > 10) {
 			cout << "Too large! Please enter a small one: ";
 		}
 		else break;
@@ -189,31 +219,50 @@ void gameProcedure(void) {
 	// create the labyrinth
 	Labyrinth* laby = new Labyrinth(labySize);
 	laby->initialize();
+	// all rooms unvisited
+	for (int i = 0; i < labySize; i++)
+		for (int j = 0; j < labySize; j++)
+			laby->m_rooms[i][j].visited = false;
 
 	// initial position 
-	hero.positionY = 0;
-	hero.positionX = (labySize + 1) / 2;
-	cout << "Welcome to the lobby. There are 3 exits: east, west, and up." << endl;
-	cout << "Enter your command: ";
+	hero.positionY = laby->m_lobbyX;
+	hero.positionX = laby->m_lobbyY;
+	cout << "Welcome to the lobby. ";
+	laby->m_rooms[hero.positionX][hero.positionY].visited = true;
 	bool withPrc = false;
 	while(1) {
+		// enter the room
+		cout << "There are " << laby->m_rooms[hero.positionX][hero.positionY].doorNum << " exit(s)";
+		for (int i = 0; i < 4; i ++) {
+			if (laby->m_rooms[hero.positionX][hero.positionY].direction[i]) {
+				cout << "    " << direction[i];
+			}
+		}
+		cout << endl << "Enter your command: ";
+
 		string cmd;
 		cin >> cmd;
-		stringstream ss(cmd);
-		ss >> cmd; ss >> cmd;
-		while (findDirectNum(cmd) == -1) {
-			cout << "Please input a legal direction, for example \"go east\":";
-			cin >> cmd;
-			ss.str(cmd); ss.clear();
-			ss >> cmd; ss >> cmd;
-		}
+		cin >> cmd;
+		while (1) {
+			while (findDirectNum(cmd) == -1) {
+				cout << "Please input a legal direction, for example \"go east\":";
+				cin >> cmd; cin >> cmd;
+			}
+			
+			if (!laby->m_rooms[hero.positionX][hero.positionY].direction[findDirectNum(cmd)]) {
+				cout << "This direction is Wall!" << endl;
+				cin >> cmd; cin >> cmd;
+				continue;
+			}
 
-		// move
-		switch(findDirectNum(cmd)) {
-			case 0: hero.positionY++; break;
-			case 1: hero.positionY--; break;
-			case 2: hero.positionX--; break;
-			case 3: hero.positionX++; break;
+			// move
+			switch(findDirectNum(cmd)) {
+				case 0: hero.positionX--; break;
+				case 1: hero.positionX++; break;
+				case 2: hero.positionY--; break;
+				case 3: hero.positionY++; break;
+			}
+			break;
 		}
 
 		// if visited
@@ -223,47 +272,39 @@ void gameProcedure(void) {
 
 			// different room types
 			if (laby->m_rooms[hero.positionX][hero.positionY].roomType == monster) {
-				cout << "Damn! A monster! You dead.";
-				cout << "Game Over";
+				cout << "Damn! A monster! You dead. ";
+				cout << "Game Over" << endl;
 				return ;
 			}
 			else if (laby->m_rooms[hero.positionX][hero.positionY].roomType == princess) {
-				cout << "Congratulation! You find the secret room!" << endl;
+				cout << "Congratulation! You find the secret room! " << endl;
+				withPrc = true;
 
 				// communicate with princess
-				cout << "You see the princess. Enter to continue:" << endl;
+				cout << "You see the princess. Enter to continue: \n";
 				getline(cin, cmd);
-				cout << "You: I will bring you out here, my princess." << endl;
+				cout << "You: I will bring you out here, my princess .";
 				getline(cin, cmd);
-				cout << "Princess: I appreciate it. Thank you so much." << endl;
+				cout << "Princess: I appreciate it. Thank you so much .";
 				getline(cin, cmd);
-				cout << "You: No time to wait. Let's get out now." << endl;
+				cout << "You: No time to wait. Let's get out now. ";
 				getline(cin, cmd);
 			}
 		}
 		else {
 			if (laby->m_rooms[hero.positionX][hero.positionY].roomType == lobby) {
 				if (withPrc) {
-					cout << "You bring the princess out of the labyrinth! You've won the game!";
+					cout << "You bring the princess out of the labyrinth! You've won the game! ";
 					return ;
 				}
 				else {
-					cout << "You've back to the lobby, but only yourself. Where is the princess?";
+					cout << "You've back to the lobby, but only yourself. Where is the princess? ";
 					getline(cin, cmd);
 				}
 			} 
 			else 
 				cout << "You've entered a familiar room. ";
 		}
-
-		// Go to next room.
-		cout << "There are " << laby->m_rooms[hero.positionX][hero.positionY].doorNum << " exit(s)";
-		for (int i = 0; i < 4; i ++) {
-			if (laby->m_rooms[hero.positionX][hero.positionY].direction[i]) {
-				cout << "    " << direction[i];
-			}
-		}
-		cout << endl << "Enter your command: ";
 	}
 }
 
@@ -272,11 +313,10 @@ int main() {
 	unsigned seed;
 	seed = time(0);
 	srand(seed);
-
-	cout << "Welcome to Save the Princess." << endl;
-
 	// game repeat
 	while(1) {
+		system("CLS");
+		cout << "Welcome to Save the Princess." << endl;
 		gameProcedure();
 		cout << "Do you want to play again? [Yes(Y)/No(N)]:";
 		string again;
