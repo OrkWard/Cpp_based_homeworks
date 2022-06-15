d990c7093a518f604ee18d8881402af180e2b22e
OrkWard <orkward_o@outlook.com>
2022-06-15 16:28:55 +0800

    finish project 7

--- Object Orientation Programming/project 7/Allocator.h
+++ Object Orientation Programming/project 7/Allocator.h
@@ -1,62 +1,224 @@
-#include <iostream>
 #include <stdlib.h>
-#define BASEPOOLCOUNT 16ULL
+#include <iostream>
+
+namespace myAllocator {
+
+constexpr auto BASEPOOLCOUNT = 16ULL;
 
 /* Implementation of Memory Pool
- * Maintain a internal linked list to store a series of memory blocks inscresing 
- * in capacity providing available memory. Each block contains a linked list which
- * trace the memory already allocated, a node pointer to free memory.
- * When an allocation required, the pool traverse the free node list to check
- * whether there are enough memory in certain block, return the node pointer if 
- * found or use malloc() two times as large as the max size of maintained blocks, 
- * then check the capacity again. If required memory is larger than 64kb, just use
- * malloc() to allocate the memory and package it into a node.
+ * Maintain a list of free nodes in a continuous memory block. Nodes won't
+ * always have the same size. When asked to allocate a object, the allocator
+ * pick a memory block from the first node of the list. If the size of the 
+ * node larger than 1, just reduce the size of it. If the pool capacity is 
+ * not enough, use malloc() to add another block with the same size of the
+ * capacity now. If not, remove the node from the list. When asked to 
+ * deallocate a object, the allocator package the given pointer to a node 
+ * and insert it into the list.
  */
 
-template<typename value_type>
+template<typename _Ty>
 class Allocator {
 public:
+    // types required
+    typedef _Ty value_type;
     typedef value_type *pointer;
     typedef const value_type *const_pointer;
     typedef value_type& reference;
     typedef const value_type& const_reference;
+    typedef size_t size_type;
+    typedef ptrdiff_t difference_type;
+    template <typename _other>
+    struct rebind { typedef Allocator<_other> other; };
     
-    // these types works for memory pool
-    struct node {
-        node *next;
-        pointer lump;
-        size_t size;
-        // TODO
-    };
-    typedef node *node_pointer;
-    struct block {
-        block *next;
-        node_pointer node_head;
-        node_pointer free_node;
-    };
-    typedef block *block_pointer;
-
-    // core function
-    pointer allocate(size_t count);
-    void deallocate(pointer ptr, size_t count);
-
-    // construct/destruct object with arguments
-    template<typename Objty, typename ...Types>
-    void construct(Objty *ptr, Types&& ...args);
-    template<typename Uty> void destroy(Uty *ptr);
-
+    // functions
     Allocator();
+    Allocator(const Allocator& allocator);
+    Allocator(Allocator&& allocator);
+    template <typename _other>
+    Allocator(const Allocator<_other>& allocator);
+
     ~Allocator();
-    pointer address(reference val) const;
-    const_pointer address(const_reference val) const;
+
+    pointer allocate(size_type count);
+    void deallocate(pointer ptr, size_type count);
+
+    size_type max_size() const;
+
+    pointer address(reference _Val) const;
+    const_pointer address(const_reference _Val) const;
+
+    void construct(pointer _Ptr, const_reference _Val);
+    void destory(pointer _Ptr);
 
 protected:
-    block_pointer m_block_head;
-    int m_block_number;
-    size_t m_block_capacity;
-    size_t m_base_size;
+    // this struct store a block of memory
+    struct node {
+        node(node* next, pointer block, size_type size) {
+            this->next = next;
+            this->block = block;
+            this->size = size;
+        };
+        node *next;
+        pointer block;
+        size_type size;
+    };
 
-    // this function is used to add a block to list when memory not enough
-    block_pointer add_block(void);
-    pointer push_node(block_pointer block, size_t count);
+    typedef node *node_pointer;
+    size_type m_capacity;  // store the pool capacity
+    node_pointer m_free_list_head;
+    node_pointer m_free_list_tail;
+    node_pointer m_block_list;  // store the head of every blocks allocated by malloc(), use for free()
+    node_pointer add_block();   // add a node to free node list
 };
+
+template<typename _Ty>
+Allocator<_Ty>::~Allocator() {
+    node_pointer p = m_block_list;
+    while (p != NULL) {
+        free(p->block);
+        p = p->next;
+    }
+}
+
+template<typename _Ty>
+Allocator<_Ty>::Allocator() {
+    // initialize the pool with base size defined 
+	this->m_capacity = 0;
+	this->m_free_list_head = NULL;
+	this->m_free_list_tail = NULL;
+    this->m_block_list = NULL;
+}
+
+template<typename _Ty>
+Allocator<_Ty>::Allocator(const Allocator& allocator) {
+	this->m_capacity = 0;
+	this->m_free_list_head = NULL;
+	this->m_free_list_tail = NULL;
+    this->m_block_list = NULL;
+}
+
+template<typename _Ty>
+Allocator<_Ty>::Allocator(Allocator&& allocator) {
+	this->m_capacity = 0;
+	this->m_free_list_head = NULL;
+	this->m_free_list_tail = NULL;
+    this->m_block_list = NULL;
+}
+
+template<typename _Ty>
+template<typename _other>
+Allocator<_Ty>::Allocator(const Allocator<_other>& allocator) {
+	this->m_capacity = 0;
+	this->m_free_list_head = NULL;
+	this->m_free_list_tail = NULL;
+    this->m_block_list = NULL;
+}
+
+
+template<typename _Ty> 
+typename Allocator<_Ty>::pointer Allocator<_Ty>::allocate(size_type count)
+{
+	// find a node with enough capacity
+    node_pointer pNode = m_free_list_head; 
+	while (pNode != NULL && pNode->size < count) {
+		pNode = pNode->next;
+	}
+	if (pNode == NULL) 
+		do {
+			pNode = add_block();
+		} while (pNode->size < count);
+
+	// if larger, reduce the node
+	if (pNode->size > count) {
+		pNode->size -= count;
+		return pNode->block + pNode->size;
+	}
+	// if equal, delete the node
+	else if (pNode->size == count) {
+		if (pNode->next == NULL) {
+            pointer pObj = pNode->block;
+			m_free_list_head = NULL;
+			m_free_list_tail = NULL;
+            return pObj;
+		}
+		else {
+			m_free_list_head = pNode->next;
+			pointer pObj = pNode->block;
+			delete pNode;
+			return pObj;
+		}
+	}
+	else {
+		std::cout << "what fuck?" << std::endl;
+		return NULL;
+	}
+}
+
+template<typename _Ty>
+void Allocator<_Ty>::deallocate(pointer ptr, size_type count) {
+	// insert the pointer to the list
+	if (m_free_list_tail == NULL) {
+		m_free_list_head = new node(NULL, ptr, count);
+		m_free_list_tail = m_free_list_head;
+	}
+	else {
+		m_free_list_tail->next = new node(NULL, ptr, count);
+		m_free_list_tail = m_free_list_tail->next;
+	}
+}
+
+template<typename _Ty>
+typename Allocator<_Ty>::size_type Allocator<_Ty>::max_size() const
+{
+	// sizeof(size_type)
+	return INT64_MAX;
+}
+
+template<typename _Ty>
+typename Allocator<_Ty>::pointer myAllocator::Allocator<_Ty>::address(reference _Val) const {
+	return &_Val;
+}
+
+template<typename _Ty>
+typename Allocator<_Ty>::const_pointer myAllocator::Allocator<_Ty>::address(const_reference _Val) const {
+	return &_Val;
+}
+
+template<typename _Ty>
+void myAllocator::Allocator<_Ty>::construct(pointer _Ptr, const_reference _Val) {
+	new(_Ptr) value_type(_Val);
+}
+
+template<typename _Ty>
+void myAllocator::Allocator<_Ty>::destory(pointer _Ptr) {
+	_Ptr->~value_type();
+}
+
+template<typename _Ty>
+typename Allocator<_Ty>::node_pointer Allocator<_Ty>::add_block() {
+	// get a new block
+	if (m_free_list_head == NULL) {
+		if (m_capacity == 0) {
+			m_capacity = BASEPOOLCOUNT;
+		}
+		m_free_list_head = new node(NULL, (pointer)malloc(m_capacity * sizeof(value_type)), m_capacity);
+		m_free_list_tail = m_free_list_head;
+	}
+	else {
+		m_free_list_tail->next = new node(NULL, (pointer)malloc(m_capacity * sizeof(value_type)), m_capacity);
+		m_free_list_tail = m_free_list_tail->next;
+	}
+
+	// insert it to the block list for destruction
+	if (m_block_list == NULL) {
+		m_block_list = new node(NULL, m_free_list_tail->block, m_capacity);
+	}
+	else {
+		m_block_list->next = new node(NULL, m_free_list_tail->block, m_capacity);
+		m_block_list = m_block_list->next;
+		m_capacity *= 2;
+	}
+	return m_free_list_tail;
+}
+
+}
